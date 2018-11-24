f785a37afafc499c434c85d78df422496824a28a
Geerocktricks <geerockface4@gmail.com>
2018-11-24 22:03:08 +0300

    This commit adds bootstrap

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
-# from flask import render_template
-# from app import app
+from flask import render_template
+from app import app
 
-# # Views
-# @app.route('/')
-# def index():
+# Views
+@app.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     return render_template('index.html')
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - my personal blog'
+    intro = 'Daily Blog'
+    return render_template('index.html', title = title , intro = intro)
