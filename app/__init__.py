f3d0c4447a4bde894842563240f92375c621ee2e
Geerocktricks <geerockface4@gmail.com>
2018-11-25 19:24:35 +0300

    This commit will create the Register Form class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask , flash , redirect, url_for , session ,logging
 from flask_bootstrap import Bootstrap
 from .data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
 # Initializ ing application
 app = Flask(__name__)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template,Flask , flash , redirect, url_for , session ,logging
 from app import app
 from .data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
 Articles = Articles()
 
@@ -35,3 +38,13 @@ def article(id):
     articles root page that returns its data
     '''
     return render_template('article.html' , id = id)
+
+class RegisterForm(Form):
+    name = StringField('Name' , [validators.Length(min=1, max=50)])
+    username = StringField('Username' , [validators.Length(min = 4 ,max = 25)])
+    email = StringField('Email' , [validators.Length(min = 6, max = 50)])
+    password = PasswordField('password' , [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message = 'Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
